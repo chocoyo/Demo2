c36e8fb9652363c933d4fdaba4bcf0d8d99826b0
Mike Hodges <mikehodges7@outlook.com>
2020-11-02 21:15:00 -0600

    Initial commit

---
+++ app/src/main/java/com/mhodges/demo2/Doggo.java
@@ -0,0 +1,49 @@
+package com.mhodges.demo2;
+
+public class Doggo {
+    private String name;
+    private String breed;
+    private int age;
+    private float height;
+    private float weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+}
