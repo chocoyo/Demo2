fd39c504b810afd9bd99584bf2dce598929325e6
Mike Hodges <mikehodges7@outlook.com>
2020-11-07 19:41:47 -0600

    Added Inline Comparator Example

--- app/src/main/java/com/mhodges/demo2/MainActivity.java
+++ app/src/main/java/com/mhodges/demo2/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<Doggo> doggoArrayList;
@@ -20,9 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
         doggoArrayList = new ArrayList<Doggo>();
 
-        doggoArrayList.add(new Doggo("Jack", "TerrBoy", 14, 23, 10));
-        doggoArrayList.add(new Doggo("Not jack", "TerrBoy", 14, 23, 9));
-        doggoArrayList.add(new Doggo("Still not jack", "TerrBoy", 14, 23, 11));
+        doggoArrayList.add(new Doggo("Jack", "TerrBoy", 14, 45, 10));
+        doggoArrayList.add(new Doggo("Not jack", "TerrBoy", 14, 32, 9));
+        doggoArrayList.add(new Doggo("Still not jack", "TerrBoy", 14, 16, 11));
 
         ShowItems();
     }
@@ -42,4 +43,30 @@ public class MainActivity extends AppCompatActivity {
         Collections.sort(doggoArrayList, Collections.<Doggo>reverseOrder());
         ShowItems();
     }
+
+    public void btnDifferentASC_Click(View view) {
+        Collections.sort(doggoArrayList, new Comparator<Doggo>() {
+            @Override
+            public int compare(Doggo doggo, Doggo t1) {
+                return Float.compare(doggo.getHeight(), t1.getHeight());
+            }
+        });
+
+        ShowItems();
+    }
 }
+
+//    Collections.sort(articles, new Comparator<Article>() {
+//        public int compare(Article a1, Article a2) {
+//            Date d1 = ConvertDate(a1.getPubDate());
+//            Date d2 = ConvertDate(a2.getPubDate());
+//
+//            if (d1 == null || d2 == null)
+//                return 0;
+//
+//            if (sharedPreferences.getString("sortingDirection", "asc").equals("asc"))
+//                return d2.compareTo(d1);
+//            else
+//                return d1.compareTo(d2);
+//            }
+//    });
